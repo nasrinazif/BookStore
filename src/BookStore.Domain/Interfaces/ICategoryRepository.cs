7433a5acd5849de762be74f0cf7e1f8afff947f0
nasrinazif <nasri@dal.ca>
2021-04-18 09:13:42 +0430

    IBookRepository and ICategoryRepository added

---
+++ src/BookStore.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookStore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Domain.Interfaces
+{
+    interface ICategoryRepository: IRepository<Category>
+    {
+    }
+}
