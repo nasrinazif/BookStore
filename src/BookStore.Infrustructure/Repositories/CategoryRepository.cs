158bae2452fb3d8a88bd456aeda399b94ec8a062
nasrinazif <nasri@dal.ca>
2021-04-18 13:53:23 +0430

    Book and Category Repos Added

---
+++ src/BookStore.Infrustructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Domain.Interfaces;
+using BookStore.Domain.Models;
+using BookStore.Infrustructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Infrustructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BookStoreDbContext db) : base(db)
+        {
+        }
+    }
+}
