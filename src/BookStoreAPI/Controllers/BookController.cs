afd9b540b25ae10d29f27cf4f5ee9ccfd32ce7a3
nasrinazif <nasri@dal.ca>
2021-04-20 14:29:52 +0430

    Controllers added

---
+++ src/BookStoreAPI/Controllers/BookController.cs
@@ -0,0 +1,89 @@
+﻿using AutoMapper;
+using BookStore.API.Dtos.Book;
+using BookStore.Domain.Interfaces;
+using BookStore.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : MainController
+    {
+        private readonly IBookService _bookService;
+        private readonly IMapper _mapper;
+
+        public BookController(IMapper mapper, IBookService bookService)
+        {
+            _mapper = mapper;
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var books = await _bookService.GetAll();
+
+            var booksToReturn = _mapper.Map<IEnumerable<BookResultDto>>(books);
+
+            return Ok(booksToReturn);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var book = await _bookService.GetById(id);
+
+            if (book == null) return NotFound();
+
+            var bookToReturn = _mapper.Map<BookResultDto>(book);
+
+            return Ok(bookToReturn);
+        }
+
+        [HttpGet]
+        [Route("get-books-by-category/{categoryId:int}")]
+        public async Task<IActionResult> GetBooksByCategory(int categoryId)
+        {
+            var books = await _bookService.GetBooksByCategory(categoryId);
+
+            if (!books.Any()) return NotFound();
+
+            var booksToReturn = _mapper.Map<IEnumerable<BookResultDto>>(books);
+
+            return Ok(booksToReturn);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(BookAddDto bookDto)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var book = _mapper.Map<Book>(bookDto);
+            var bookResult = await _bookService.Add(book);
+
+            if (bookResult == null) return BadRequest();
+
+            var bookToReturn = _mapper.Map<BookResultDto>(bookResult);
+
+            return Ok(bookToReturn);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, BookEditDto bookDto)
+        {
+            if (id != bookDto.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            await _bookService.Update(_mapper.Map<Book>(bookDto));
+
+            return Ok(bookDto);
+        }
+    }
+}
---
+++ src/BookStoreAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper;
+using BookStore.API.Dtos.Category;
+using BookStore.Domain.Interfaces;
+using BookStore.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : MainController
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(IMapper mapper, ICategoryService categoryService)
+        {
+            _mapper = mapper;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var categories = await _categoryService.GetAll();
+
+            var categoriesToReturn = _mapper.Map<IEnumerable<CategoryResultDto>>(categories);
+
+            return Ok(categoriesToReturn);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var category = await _categoryService.GetById(id);
+
+            if (category == null) return NotFound();
+
+            var categoryToReturn = _mapper.Map<CategoryResultDto>(category);
+
+            return Ok(categoryToReturn);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CategoryAddDto categoryToAdd)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var category = _mapper.Map<Category>(categoryToAdd);
+            var categoryResult = await _categoryService.Add(category);
+
+            if (categoryResult == null) return BadRequest();
+
+            var categoryToReturn = _mapper.Map<CategoryResultDto>(categoryResult);
+
+            return Ok(categoryToReturn);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, CategoryEditDto categoryDto)
+        {
+            if (id != categoryDto.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            var category = _mapper.Map<Category>(categoryDto);
+
+            await _categoryService.Update(category);
+
+            return Ok(categoryDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null) return NotFound();
+
+            var result = await _categoryService.Remove(category);
+
+            if (!result) return BadRequest();
+
+            return Ok();
+        }
+
+        [Route("search/{category}")]
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> Search(string category)
+        {
+            var categoriesSearchResult = await _categoryService.Search(category);
+            var categories = _mapper.Map<List<Category>>(categoriesSearchResult);
+
+            if (categories == null || categories.Count == 0)
+                return NotFound("No categories were founded");
+
+            return Ok(categories);
+        }
+    }
+}
