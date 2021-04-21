f0abe8baf82eb6330370ac4a1fc8b63c82029b72
nasrinazif <nasri@dal.ca>
2021-04-21 10:48:14 +0430

    Minor Dtos changes

--- src/BookStoreAPI/Dtos/Category/CategoryAddDto.cs
+++ src/BookStoreAPI/Dtos/Category/CategoryAddDto.cs
@@ -11,6 +11,8 @@ namespace BookStore.API.Dtos.Category
         [Required(ErrorMessage = "The field {0} is required!")]
         [StringLength(150, ErrorMessage = "The field {0} must be between {2} and {1} characters!", MinimumLength = 2)]
         public string Name { get; set; }
+
+        [StringLength(150, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 2)]
         public string CatDescription { get; set; }
 
     }
--- src/BookStoreAPI/Dtos/Category/CategoryEditDto.cs
+++ src/BookStoreAPI/Dtos/Category/CategoryEditDto.cs
@@ -15,6 +15,7 @@ namespace BookStore.API.Dtos.Category
         [StringLength(150, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 2)]
         public string Name { get; set; }
 
+        [StringLength(150, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 2)]
         public string CatDescription { get; set; }
     }
 }
